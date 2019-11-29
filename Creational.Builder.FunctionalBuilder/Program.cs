727ab00c044c5d0782cbd3364415a7455e365a5d
dodinhnguyen <nguyendo891@yahoo.com>
2019-11-29 14:28:02 +0100

    Add project files.

---
+++ Creational.Builder.FunctionalBuilder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creational.Builder.FunctionalBuilder
+{
+    public class Person
+    {
+        public string Name, Position;
+    }
+
+    public class PersonBuilder
+    {
+        public readonly List<Action<Person>> Actions = new List<Action<Person>>();
+
+        public PersonBuilder Called(string name)
+        {
+            Actions.Add(p => { p.Name = name; });
+            return this;
+        }
+
+        public Person Build()
+        {
+            var p = new Person();
+            Actions.ForEach(a => a(p));
+            return p;
+        }
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorksAsA
+          (this PersonBuilder builder, string position)
+        {
+            builder.Actions.Add(p => { p.Position = position; });
+            return builder;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            var person = pb.Called("Dmitri").WorksAsA("Programmer").Build();
+        }
+    }
+}
